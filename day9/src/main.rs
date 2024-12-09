8108caa7ffb733c4c1078c1cab39be3cb7e674ce
Jeff Powell <jrpowel9@ncsu.edu>
2024-12-09 11:46:15 -0500

    store u64s instead of strings lmao

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,38 +1,32 @@
-use core::num;
-use std::{fs::read_to_string, ops::Range, thread::sleep, time::Duration};
+use std::{fs::read_to_string, ops::Range, time::Instant};
 
 fn main() {
     let input = read_to_string("example.txt").unwrap();
     let input = read_to_string("input.txt").unwrap();
     let str = parse_input(input);
 
+    let t = Instant::now();
     println!("{}", part2(str));
+    dbg!(t.elapsed());
 }
 
-fn find_free_chunk(fs: &[String], required_size: usize) -> Option<usize> {
+fn find_free_chunk(fs: &[Option<u64>], required_size: usize) -> Option<usize> {
     fs.windows(required_size)
-        .position(|w| w.iter().all(|x| *x == EMPTY))
+        .position(|w| w.iter().all(|x| x.is_none()))
 }
 
-fn find_required_chunk(fs: &[String], num_to_find: u64) -> Range<usize> {
-    let start = fs
-        .iter()
-        .position(|s| *s == num_to_find.to_string())
-        .unwrap();
+fn find_required_chunk(fs: &[Option<u64>], num_to_find: u64) -> Range<usize> {
+    let start = fs.iter().position(|&s| s == Some(num_to_find)).unwrap();
     start
         ..start
             + fs[start..]
                 .iter()
-                .take_while(|s| **s == num_to_find.to_string())
+                .take_while(|&&s| s == Some(num_to_find))
                 .count()
 }
 
-fn part2(mut fs: Vec<String>) -> u64 {
-    let mut num_to_find = fs
-        .iter()
-        .filter_map(|x| x.parse::<u64>().ok())
-        .max()
-        .unwrap();
+fn part2(mut fs: Vec<Option<u64>>) -> u64 {
+    let mut num_to_find = fs.iter().filter_map(|&x| x).max().unwrap();
     let mut chunk = find_required_chunk(&fs, num_to_find);
     loop {
         if let Some(free) = find_free_chunk(&fs[..chunk.start], chunk.len()) {
@@ -49,25 +43,25 @@ fn part2(mut fs: Vec<String>) -> u64 {
 
     fs.into_iter()
         .enumerate()
-        .filter_map(|(idx, c)| c.parse::<u64>().ok().map(|x| x * idx as u64))
+        .filter_map(|(idx, c)| c.map(|x| x * idx as u64))
         .sum::<u64>()
 }
 
-fn find_free(fs: &[String], start: usize) -> usize {
+fn find_free(fs: &[Option<u64>], start: usize) -> usize {
     fs.iter()
         .enumerate()
         .skip(start)
-        .find(|c| *c.1 == EMPTY)
+        .find(|c| c.1.is_none())
         .map(|c| c.0)
         .unwrap()
 }
 
-fn find_allocated(fs: &[String]) -> usize {
-    fs.len() - 1 - fs.iter().rev().position(|c| c != EMPTY).unwrap()
+fn find_allocated(fs: &[Option<u64>]) -> usize {
+    fs.len() - 1 - fs.iter().rev().position(|c| c.is_some()).unwrap()
 }
 
 #[allow(dead_code)]
-fn part1(mut fs: Vec<String>) -> u64 {
+fn part1(mut fs: Vec<Option<u64>>) -> u64 {
     let mut free = find_free(&fs, 0);
     let mut alloc = find_allocated(&fs);
     while free < alloc {
@@ -78,30 +72,24 @@ fn part1(mut fs: Vec<String>) -> u64 {
 
     fs.into_iter()
         .enumerate()
-        .filter_map(|(idx, c)| c.parse::<u64>().ok().map(|x| x * idx as u64))
+        .filter_map(|(idx, c)| c.map(|x| x * idx as u64))
         .sum::<u64>()
 }
 
-fn parse_input(input: String) -> Vec<String> {
+fn parse_input(input: String) -> Vec<Option<u64>> {
     let mut vec = vec![];
     let mut file = true;
     let mut file_num = 0;
 
     for num in input.trim().chars() {
         if file {
-            vec.extend(
-                std::iter::repeat(file_num.to_string()).take(num.to_digit(10).unwrap() as usize),
-            );
+            vec.extend(std::iter::repeat(Some(file_num)).take(num.to_digit(10).unwrap() as usize));
             file_num += 1;
         } else {
-            vec.extend(
-                std::iter::repeat(EMPTY.to_string()).take(num.to_digit(10).unwrap() as usize),
-            );
+            vec.extend(std::iter::repeat(None).take(num.to_digit(10).unwrap() as usize));
         }
         file = !file;
     }
 
     vec
 }
-
-const EMPTY: &str = ".";
