b88f723f963c32c1c1ba66de1d0411c88e622b1b
Jeff Powell <jeffersonrpowell@gmail.com>
2024-12-18 12:51:08 -0500

    its hacky but works...

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,49 +1,90 @@
 use pathfinding::prelude::*;
+use rayon::iter::{IntoParallelIterator, ParallelBridge, ParallelIterator};
 use std::{
     collections::{HashMap, HashSet},
     fs::File,
     io::read_to_string,
 };
 
-//const INPUT: &str = "input.txt";
-//const GRID_DIM: usize = 71;
-const INPUT: &str = "example.txt";
-const GRID_DIM: usize = 7;
+const INPUT: &str = "input.txt";
+const GRID_DIM: usize = 71;
+const TAKE: usize = 1024;
+//const INPUT: &str = "example.txt";
+//const GRID_DIM: usize = 7;
+//const TAKE: usize = 12;
 
 fn main() {
     let str = read_to_string(File::open(INPUT).unwrap()).unwrap();
     let grids = parse_input(&str);
 
     println!("{}", part1(&grids));
+    println!("{:?}", part2(&grids));
 }
 
-fn part1(grids: &[(usize, usize)]) -> usize {
+fn part2(blockers: &[(usize, usize)]) -> (usize, usize) {
     let start = State {
         pos: (0, 0),
         time: 0,
     };
 
-    let successors = |s: &State| neighbors(grids, *s);
+    let heuristic =
+        |s: &State| ((s.pos.0.abs_diff(GRID_DIM - 1)) + (s.pos.1.abs_diff(GRID_DIM - 1))) as u32;
+    let success = |s: &State| s.pos == (GRID_DIM - 1, GRID_DIM - 1);
+
+    (2587..).par_bridge().into_par_iter().for_each(|i| {
+        let successors = |s: &State| neighbors(&blockers[..i], *s);
+        // Just look for the last "searching" message that doesn't have a corresponding "successful" message lmao.
+        println!("Searching {i}: {:?}", blockers[i - 1]);
+        if astar(&start, successors, heuristic, success).is_none() {
+            println!("------------FOUND: {:?}", blockers[i - 1],);
+        }
+        println!("{i} successful");
+    });
+    for i in 2587.. {
+        let successors = |s: &State| neighbors(&blockers[..i], *s);
+        if astar(&start, successors, heuristic, success).is_none() {
+            return blockers[i];
+        }
+        println!("{i} successful");
+    }
+    unreachable!()
+}
+
+fn part1(blockers: &[(usize, usize)]) -> usize {
+    let start = State {
+        pos: (0, 0),
+        time: 0,
+    };
+    let blockers = &blockers[0..TAKE];
+
+    draw_path(vec![], blockers);
+    let successors = |s: &State| neighbors(blockers, *s);
     let heuristic =
         |s: &State| ((s.pos.0.abs_diff(GRID_DIM - 1)) + (s.pos.1.abs_diff(GRID_DIM - 1))) as u32;
     let success = |s: &State| s.pos == (GRID_DIM - 1, GRID_DIM - 1);
     //let (paths, cost) = astar_bag_collect(&start, successors, heuristic, success).unwrap();
     let (path, cost) = astar(&start, successors, heuristic, success).unwrap();
-    draw_path(path);
+    draw_path(path, blockers);
     cost as usize
 }
 
-fn draw_path(path: Vec<State>) {
+fn draw_path(path: Vec<State>, blockers: &[(usize, usize)]) {
+    //dbg!(&path);
+    //dbg!(blockers);
+    //assert!(path.iter().map(|s| s.pos).all(|p| !blockers.contains(&p)));
     for y in 0..GRID_DIM {
         for x in 0..GRID_DIM {
             if path.iter().any(|s| s.pos == (x, y)) {
                 print!("O");
+            } else if blockers.contains(&(x, y)) {
+                print!("#");
             } else {
                 print!(".");
             }
         }
         println!();
     }
+    println!();
 }
 
 fn neighbors(blockers: &[(usize, usize)], State { pos, time }: State) -> Vec<(State, u32)> {
@@ -55,7 +96,10 @@ fn neighbors(blockers: &[(usize, usize)], State { pos, time }: State) -> Vec<(St
         let Some(new_y) = pos.1.checked_add_signed(offset.1) else {
             continue;
         };
-        if blockers[0..time].contains(&(new_x, new_y)) {
+        if new_y >= GRID_DIM || new_x >= GRID_DIM {
+            continue;
+        }
+        if blockers.contains(&(new_x, new_y)) {
             continue;
         }
         possible_neighbors.push((
@@ -76,7 +120,7 @@ struct State {
 }
 
 fn parse_input(str: &str) -> Vec<(usize, usize)> {
-    let mut ret = vec![(usize::MAX, usize::MAX)];
+    let mut ret = vec![];
     for line in str.lines() {
         let (x, y) = line.split_once(',').unwrap();
         ret.push((x.parse().unwrap(), y.parse().unwrap()));
